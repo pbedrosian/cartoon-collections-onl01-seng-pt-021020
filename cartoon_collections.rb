7af75e1ab8a5157359ad3653a3f32a93e3e2cdd7
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-02-19 13:33:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,9 +13,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any? do |test|
-    test.length == 4
-  end
+  array.any? {|word| word.length < 4}
 end
 
 def find_the_cheese# code an argument here
