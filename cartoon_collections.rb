8667671daea2c0e23376b5b1c0863ff288b1a793
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-02-19 13:16:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,8 @@ def long_planeteer_calls(array,testing)
     test.length > 4
   end
   testing do | num |
-    num.length > 4 
+    num.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
