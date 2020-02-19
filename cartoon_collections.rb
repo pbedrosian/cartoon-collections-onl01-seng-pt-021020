7f41202aaf9ee2ca21c85a7dde2da37d627d6fb6
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-02-19 12:45:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,8 @@ end
 
 def summon_captain_planet(array)
   array.map do
-  |test| p "#{test.capitalize}!"
+    |test| p "#{test.capitalize}!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
